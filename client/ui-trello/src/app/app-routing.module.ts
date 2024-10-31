e5842256c419f8ae28db60880a3c681f2070b14e
Dora Toth <jolewiena068@gmail.com>
2024-10-31 14:54:14 +0100

    Added form for creating projects

--- client/ui-trello/src/app/app-routing.module.ts
+++ client/ui-trello/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RegistrationComponent} from "./registration/registration.component";
+import {AddProjectComponent} from "./add-project/add-project.component";
 
 export const routes: Routes = [  { path: 'register', component: RegistrationComponent },
-  { path: '', redirectTo: '/register', pathMatch: 'full' }];
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: 'projects', component: AddProjectComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- server/user-service/go.mod
+++ server/user-service/go.mod
@@ -4,6 +4,7 @@ go 1.22.1
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
 	go.mongodb.org/mongo-driver v1.17.1
@@ -13,7 +14,6 @@ require (
 require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
