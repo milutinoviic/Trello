a1b56b0416087d9cbc557ad559005091fd6942bf
Dora Toth <jolewiena068@gmail.com>
2024-11-02 14:16:16 +0100

    Roles added, select manager for project

--- server/user-service/data/user.go
+++ server/user-service/data/user.go
@@ -2,13 +2,16 @@ package data
 
 import (
 	"context"
+	"encoding/json"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io"
 )
 
 type AccountRequest struct {
 	Email     string `bson:"email" json:"email"`
 	FirstName string `bson:"first_name" json:"first_name"`
 	LastName  string `bson:"last_name" json:"last_name"`
+	Role      string `bson:"role" json:"role"`
 }
 
 type Account struct {
@@ -17,8 +20,16 @@ type Account struct {
 	FirstName string             `bson:"first_name" json:"first_name"`
 	LastName  string             `bson:"last_name" json:"last_name"`
 	Password  string             `bson:"password" json:"password"`
+	Role      string             `bson:"role" json:"role"`
 }
+
+type Accounts []*Account
 
 type AccountRepository interface {
 	Registration(request *AccountRequest, ctx context.Context) error
+}
+
+func (a *Accounts) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(a)
 }
