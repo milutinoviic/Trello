996cee7dc488ebf03fc8ccf3213211b778dc4fa0
Stefan Milutinovic <milutinovic.stefan2003@gmail.com>
2024-11-03 12:43:01 +0100

    Done part for supplying all users on the backend

--- client/ui-trello/src/app/models/project.model.ts
+++ client/ui-trello/src/app/models/project.model.ts
@@ -3,13 +3,15 @@ export class Project {
   end_date: Date;
   min_members: number;
   max_members: number;
+  userIDs : string[];
 
 
 
-  constructor(project_name: string, endDate: Date, minMember: number, maxMember: number) {
+  constructor(project_name: string, endDate: Date, minMember: number, maxMember: number,userIDs: string[] = []) {
     this.name = project_name;
     this.end_date = endDate;
     this.min_members = minMember;
     this.max_members = maxMember;
+    this.userIDs = userIDs;
   }
 }
