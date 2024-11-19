fe98f598663f63d7cf235344c91550ab7a4747eb
Dora Toth <jolewiena068@gmail.com>
2024-11-19 12:43:33 +0100

    Merge to develop

--- client/ui-trello/src/app/guards/role.guard.ts
+++ client/ui-trello/src/app/guards/role.guard.ts
@@ -6,18 +6,18 @@ export const roleGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
   const router = inject(Router);
 
-  const role = localStorage.getItem("role");
+  // const role = localStorage.getItem("role");
   const expectedRoles: string = route.data['expectedRoles'];
 
-  console.log("roleee " + role);
+  // console.log("roleee " + role);
   console.log("userId je " + accountService.getUserId());
   console.log("expectedRoles " + expectedRoles);
   const roles: string[] = expectedRoles.split("|");
 
-  if (roles.indexOf(role || "") === -1) {
-    router.navigate(["/forbidden"]);
-    return false;
-  }
+  // if (roles.indexOf(role || "") === -1) {
+  //   router.navigate(["/forbidden"]);
+  //   return false;
+  // }
 
   return true;
 };
